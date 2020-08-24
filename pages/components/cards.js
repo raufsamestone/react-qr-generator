94f21ef8386ba0c0c1377eff314dd3772a819f36
Berkay <berkaydemirbas@yahoo.com>
2020-08-24 17:52:07 +0300

    added card URL

--- pages/components/cards.js
+++ pages/components/cards.js
@@ -1,8 +1,9 @@
 export default function CardImages() {
   const cardImage = "/demo-ss.png";
+  const url = "https://react-qr-generator.vercel.app/"
   return (
     <>
-      <meta property="og:url" content={cardImage} />
+      <meta property="og:url" content={url} />
       <meta property="og:image" content={cardImage} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@raufsamestone" />
